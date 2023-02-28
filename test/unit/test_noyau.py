ab6c13a8c8fd90f534f30c024eb4041939ed6179
Yoann Bonnaventure <yo.bonnaventure@gmail.com>
2023-02-28 09:47:24 +0100

    Ajout des tests unitaires

--- test/unit/test_noyau.py
+++ test/unit/test_noyau.py
@@ -4,3 +4,10 @@ from noyau import *
 
 def test_changer_joueur():
    assert True==True
+
+def test_jeton():
+   assert(jeton('Hardy',2,3,1,2,grille)) != 0
+
+@pytest.mark.parametrize("n1, n2, n3",  [(1,2,3), (1,2,1), message])
+def test_jouer(n1,n2,n3):
+    assert(jouer(n1, n2, n3)) == True
